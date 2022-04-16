a80aab089f5728f2d0c4c1a02768f510c850bfd6
Terric-Tech <ferrictech@gmail.com>
2022-04-16 13:41:30 +0200

    app-rounting: Implimentation of app routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,5 +9,7 @@ import { Router } from '@angular/router';
 export class AppComponent {
   title = 'e-questrian';
 
-  constructor(public router: Router) {}
+  constructor(public router: Router) {
+    router.navigate(['/home']);
+  }
 }
