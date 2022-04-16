321154476787dbebc0b6922a1c9fc8c84b201476
Terric-Tech <ferrictech@gmail.com>
2022-04-16 13:46:44 +0200

    pages/home: Adding routing for calander button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalanderComponent } from './pages/calander/calander.component';
 import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [{ path: 'home', component: HomeComponent }];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'calander', component: CalanderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
