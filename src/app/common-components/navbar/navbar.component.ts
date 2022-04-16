906d7f39786a21cbae3c9b2f16095f7ff11e54df
Terric-Tech <ferrictech@gmail.com>
2022-04-16 16:16:00 +0200

    common-components/navbar: Dynamic menu options implimented

--- src/app/common-components/navbar/navbar.component.ts
+++ src/app/common-components/navbar/navbar.component.ts
@@ -1,6 +1,17 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+export enum MenuOptions {
+  CALENDAR,
+  FINANCES,
+  CLIENTS,
+}
+
+export interface MenuOption {
+  display: string;
+  option: MenuOptions;
+}
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -10,6 +21,12 @@ export class NavbarComponent implements OnInit {
   innerWidth: number | undefined;
   isMobileView: boolean = false;
   displayMenu: boolean = true;
+
+  readonly menuOptions: MenuOption[] = [
+    { display: 'Calendar', option: MenuOptions.CALENDAR },
+    { display: 'Finances', option: MenuOptions.FINANCES },
+    { display: 'Clients', option: MenuOptions.CLIENTS },
+  ];
 
   @HostListener('window:resize', ['$event'])
   onResize() {
@@ -28,6 +45,22 @@ export class NavbarComponent implements OnInit {
 
   onMenuIconClick() {
     this.displayMenu = !this.displayMenu;
+  }
+
+  onMenuOptionClicked(option: MenuOptions) {
+    switch (option) {
+      case MenuOptions.CALENDAR: {
+        this.router.navigate(['/calendar']);
+        break;
+      }
+      case MenuOptions.FINANCES: {
+        this.router.navigate(['/finances']);
+        break;
+      }
+      case MenuOptions.CLIENTS: {
+        this.router.navigate(['/clients']);
+      }
+    }
   }
 
   private determineView() {
