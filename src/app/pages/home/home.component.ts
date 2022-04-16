e98997fa96881b41aff573bacd4d2663cae6a1ab
Terric-Tech <ferrictech@gmail.com>
2022-04-16 17:06:40 +0200

    pages/home: Improvement to button styling/layout and navigation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,25 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+export enum MenuOptions {
+  CALENDAR,
+  FINANCES,
+  CLIENTS,
+}
+
+export interface MenuOption {
+  display: string;
+  option: MenuOptions;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  readonly menuOptions: MenuOption[] = [
+    { display: 'Calendar', option: MenuOptions.CALENDAR },
+    { display: 'Finances', option: MenuOptions.FINANCES },
+    { display: 'Clients', option: MenuOptions.CLIENTS },
+  ];
+
   constructor(public router: Router) {}
 
   ngOnInit(): void {}
 
-  oncalendarClick() {
-    this.router.navigate(['/calendar']);
-  }
-
-  onFinancesClick() {
-    this.router.navigate(['/finances']);
-  }
-
-  onClientsClick() {
-    this.router.navigate(['/clients']);
+  onMenuOptionClicked(option: MenuOptions) {
+    switch (option) {
+      case MenuOptions.CALENDAR: {
+        this.router.navigate(['/calendar']);
+        break;
+      }
+      case MenuOptions.FINANCES: {
+        this.router.navigate(['/finances']);
+        break;
+      }
+      case MenuOptions.CLIENTS: {
+        this.router.navigate(['/clients']);
+      }
+    }
   }
 }
