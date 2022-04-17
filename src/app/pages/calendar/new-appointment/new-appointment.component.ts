f445bb0942e1518929407cbfe16fcf4773db567b
Terric-Tech <ferrictech@gmail.com>
2022-04-17 18:13:01 +0200

    pages/calendar/new-appointment: Form now defaults with times based on blocked clicked

--- src/app/pages/calendar/new-appointment/new-appointment.component.ts
+++ src/app/pages/calendar/new-appointment/new-appointment.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-new-appointment',
@@ -9,16 +10,56 @@ export class NewAppointmentComponent implements OnInit {
   @Input() startTime: string = '';
   @Output() canceled = new EventEmitter<void>();
 
-  times = ['8:00', '8:30'];
+  newAppoitmentForm = new FormGroup({
+    title: new FormControl('New appointment'),
+    startTime: new FormControl(''),
+    endTime: new FormControl(''),
+  });
+
+  times: string[] = [];
   displayTime = '';
 
-  constructor() {
-    this.displayTime = this.startTime;
+  constructor() {}
+
+  ngOnInit(): void {
+    this.setCalendarBlocks();
+    this.newAppoitmentForm.controls['startTime'].setValue(this.startTime);
+    let endTime = this.determineEndTime(this.startTime);
+    this.newAppoitmentForm.controls['endTime'].setValue(endTime);
   }
 
-  ngOnInit(): void {}
+  onStartTimeSelected(time: string) {
+    // Delete all the times before this
+  }
+
+  onSubmit() {}
 
   onCancelClick() {
     this.canceled.emit();
+  }
+
+  private determineEndTime(startTime: string) {
+    let hour = startTime.substring(0, startTime.indexOf(':'));
+    let minutes = startTime.substring(
+      startTime.indexOf(':') + 1,
+      startTime.length
+    );
+    let endHour = '';
+    let endMinutes = '';
+    if (minutes == '30') {
+      endHour = (parseInt(hour, 10) + 1).toString();
+      endMinutes = '00';
+    } else {
+      endHour = hour;
+      endMinutes = '30';
+    }
+    return endHour + ':' + endMinutes;
+  }
+
+  private setCalendarBlocks() {
+    for (let i = 6; i < 18; i++) {
+      this.times.push(i + ':00');
+      this.times.push(i + ':30');
+    }
   }
 }
