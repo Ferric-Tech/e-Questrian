6b19df3294a8924ea69f29be58e94eb2defe26d8
Terric-Tech <ferrictech@gmail.com>
2022-04-19 13:05:21 +0200

    pages/home: Menu options made more dynamic

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-export enum MenuOptions {
-  CALENDAR,
-  FINANCES,
-  CLIENTS,
-}
-
 export interface MenuOption {
   display: string;
-  option: MenuOptions;
+  path: string;
 }
 
 @Component({
@@ -19,28 +13,16 @@ export interface MenuOption {
 })
 export class HomeComponent implements OnInit {
   readonly menuOptions: MenuOption[] = [
-    { display: 'Calendar', option: MenuOptions.CALENDAR },
-    { display: 'Finances', option: MenuOptions.FINANCES },
-    { display: 'Clients', option: MenuOptions.CLIENTS },
+    { display: 'Calendar', path: '/calendar' },
+    { display: 'Finances', path: '/finances' },
+    { display: 'Clients', path: '/clients' },
   ];
 
   constructor(public router: Router) {}
 
   ngOnInit(): void {}
 
-  onMenuOptionClicked(option: MenuOptions) {
-    switch (option) {
-      case MenuOptions.CALENDAR: {
-        this.router.navigate(['/calendar']);
-        break;
-      }
-      case MenuOptions.FINANCES: {
-        this.router.navigate(['/finances']);
-        break;
-      }
-      case MenuOptions.CLIENTS: {
-        this.router.navigate(['/clients']);
-      }
-    }
+  onMenuOptionClicked(path: string) {
+    this.router.navigate([path]);
   }
 }
